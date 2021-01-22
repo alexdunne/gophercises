610bce5eb6f3ac9b06a9a6033219c3d04451addc
Alex Dunne <hi@alexdunne.net>
2021-01-22 23:39:12 +0000

    Added serving the intro chapter of the story

--- choose-your-own-adventure/cmd/cyoaweb/main.go
+++ choose-your-own-adventure/cmd/cyoaweb/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"cyoa"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "Web default port")
 	filename := flag.String("file", "gopher.json", "The story JSON file")
 	flag.Parse()
 	fmt.Printf("Story %s loaded\n", *filename)
@@ -24,7 +27,9 @@ func main() {
 		exit(fmt.Sprintf("Something went wrong whilst decoding the file %s", *filename))
 	}
 
-	fmt.Printf("%+v\n", story)
+	handler := cyoa.NewHandler(story)
+	fmt.Printf("Starting port on :%d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), handler))
 }
 
 func exit(msg string) {
---
+++ choose-your-own-adventure/story.go
@@ -0,0 +1,74 @@
+package cyoa
+
+import (
+	"encoding/json"
+	"html/template"
+	"io"
+	"net/http"
+)
+
+var defaultHandlerTemplate = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Choose Your Own Adventure</title>
+</head>
+<body>
+  <h1>{{.Title}}</h1>
+  {{range .Paragraphs}}
+  <p>{{.}}</p>
+  {{end}}
+
+  <ul>
+  {{range .Options}}
+    <li>
+      <a href="/{{.Chapter}}">{{.Text}}</a>
+    </li>
+  {{end}}
+  </ul>
+</body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	storyTemplate := template.Must(template.New("").Parse(defaultHandlerTemplate))
+
+	err := storyTemplate.Execute(w, h.s["intro"])
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func JsonStory(r io.Reader) (Story, error) {
+	decoder := json.NewDecoder(r)
+
+	var story Story
+	if err := decoder.Decode(&story); err != nil {
+		return nil, err
+	}
+
+	return story, nil
+}
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title      string          `json:"title"`
+	Paragraphs []string        `json:"story"`
+	Options    []ChapterOption `json:"options"`
+}
+
+type ChapterOption struct {
+	Text    string `json:"text"`
+	Chapter string `json:"arc"`
+}
