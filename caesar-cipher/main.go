b5abc2f8208ef8affcd28e99244810f92038ba22
Alex Dunne <hi@alexdunne.net>
2021-01-24 18:52:43 +0000

    Refactor caeser cipher to check against the ASCII code value rather than checking for the position in an alphabet

--- caesar-cipher/main.go
+++ caesar-cipher/main.go
@@ -11,38 +11,24 @@ func main() {
 	fmt.Scanf("%s\n", &input)
 	fmt.Scanf("%d\n", &cipherKey)
 
-	alphabetLower := "abcdefghijklmnopqrstuvwxyz"
-	alphabetUpper := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
 	output := ""
 
 	for _, char := range input {
 		switch {
-		case findOriginalPosition(alphabetLower, char) != -1:
-			pos := findOriginalPosition(alphabetLower, char)
-			newPos := (pos + cipherKey) % len(alphabetLower)
-
-			output += string(alphabetLower[newPos])
-		case findOriginalPosition(alphabetUpper, char) != -1:
-			pos := findOriginalPosition(alphabetUpper, char)
-			newPos := (pos + cipherKey) % len(alphabetUpper)
-
-			output += string(alphabetUpper[newPos])
+		case char >= 'A' && char <= 'Z':
+			output += string(rotate('A', char, cipherKey))
+		case char >= 'a' && char <= 'z':
+			output += string(rotate('a', char, cipherKey))
 		default:
 			output += string(char)
-
 		}
 	}
 
 	fmt.Println(output)
 }
 
-func findOriginalPosition(alphabet string, value rune) int {
-	for i, letter := range alphabet {
-		if letter == value {
-			return i
-		}
-	}
-
-	return -1
+func rotate(base int, r rune, cipherKey int) rune {
+	tmp := int(r) - base
+	tmp = (tmp + cipherKey) % 26
+	return rune(tmp + base)
 }
